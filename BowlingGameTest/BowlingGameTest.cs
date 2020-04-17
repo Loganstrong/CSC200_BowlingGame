5ba18c45f47fdc792b7f7670d3c826a31e5deb78
Logan Armstrong <Logan.Armstrong@stmartin.edu>
2020-04-17 16:16:05 -0700

    Add project files.

---
+++ BowlingGameTest/BowlingGameTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using CSC200_BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameTest
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+        [TestMethod]
+        public void AllGutterGameScoreShouldBeZero()
+        {
+            Game game = new Game();
+          
+            RollsMany(game, 20, 0);
+                                  
+            Assert.AreEqual(0, game.Score());
+        }
+
+        private void RollMany(Game game, int rolls, int pins)
+        {
+            for (int roll = 0; roll < rolls ; roll++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+
+        [TestMethod]
+        public void AllOnesGameShouldScoreTwenty()
+        {
+            Game game = new Game();
+
+            RollMany(game, 20, 1);
+            
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [TestMethod]
+        public void SpareShouldGetNextRollBonus()
+        {
+            Game game = new Game();
+            game.Roll(2);
+            game.Roll(8);
+            game.Roll(6);
+            RollsMany(game, 18, 0);
+            Assert.AreEqual(22, game.Score());
+        }
+
+
+        [TestMethod]
+        public void PerfectGameShouldGetPerfectScore()
+        {
+            Game game = new Game();
+            RollMany(game, 12, 10);
+            Assert.AreEqual(300, game.Score());
+
+        }
+
+
+        [TestMethod]
+        public void StrikeShouldGetTwoRollBonus()
+        {
+            Game game = new Game();
+            game.Roll(10);
+            game.Roll(2);
+            game.Roll(7);
+
+
+            RollsMany(game, 14, 0);
+            Assert.AreEqual(28, game.Score());
+        }
+        private void RollsMany(Game game, int rolls, int pins)
+        {
+            for(int index = 0; index < rolls; index++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+
+        
+    }
+}
---
+++ CSC200_BowlingGame/Game.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC200_BowlingGame
+{
+    public class Game
+    { 
+        private int[] _rolls = new int[21];
+        private int _turn = 0;
+
+
+        public void Roll(int pins)
+        {
+            
+            _rolls[_turn++] = pins;
+            
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int roll = 0;
+            
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if(_rolls[roll] == 10) // strike
+                {
+                    score += 10 + _rolls[roll + 1] + _rolls[roll + 2];
+                    roll += 1;
+                }
+
+                else if (IsSpareFrame(roll)) //spare
+                {
+                    score += 10 + _rolls[roll + 2];
+                    roll += 2;
+                }
+                else
+                {
+                    score += _rolls[roll] + _rolls[roll + 1];
+                    roll += 2;
+                }
+               
+            }
+
+            return score;
+        }
+
+        private bool IsSpareFrame(int roll)
+        {
+            return _rolls[roll] + _rolls[roll + 1] == 10;
+        }
+
+            //for (int roll = 0; roll < _rolls.Length; roll++)
+            //{
+            //    score += _rolls[roll];
+            //}
+
+           // return score;
+        
+    }
+}
